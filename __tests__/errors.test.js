0f7bd036f6815f1ccb1ca0b9787b9d3237117ec2
Tia Low <lowtia@gmail.com>
2020-08-19 19:05:08 -0700

    Wrote routes, passing error tests

--- __tests__/errors.test.js
+++ __tests__/errors.test.js
@@ -1,31 +1,26 @@
 'use strict';
 
-const server = require('../lib/server.js');
+const {server} = require('../lib/server.js');
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 
 describe('404 Error Handling', () => {
 
   it('should respond with a 404 on an invalid route', () => {
-
     return mockRequest
       .get('/invalid')
       .then( results=> {
         expect(results.status).toBe(404);
       }).catch(console.error);
-
   });
 
   it('should respond with a 404 on an invalid method', () => {
-
     return mockRequest
       .post('/')
       .then( results=> {
         expect(results.status).toBe(404);
       }).catch(console.error);
-      
   });
-  
   
 });
 
@@ -34,13 +29,11 @@ describe('404 Error Handling', () => {
 describe('500 Error Handling', () => {
   
   it('should respond with a 500 on a server error', () => {
-  
     return mockRequest
       .get('/badroute')
       .then(results => {
         expect(results.status).toBe(500);
       }).catch(console.error);
-
   });
   
 });
@@ -49,23 +42,19 @@ describe('500 Error Handling', () => {
 describe('Sunny Day - 200 Handling', () => {
 
   it('should respond with 200 on proper request to categories', () => {
-
     return mockRequest
       .get('/api/v1/categories')
       .then(results => {
         expect(results.status).toBe(200);
       }).catch(console.error);
-
   });
 
   it('should respond with 200 on proper request to products', () => {
-
     return mockRequest
       .get('/api/v1/products')
       .then(results => {
         expect(results.status).toBe(200);
       }).catch(console.error);
-
   });
 
 });
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// gonna use mongoose now
+// const mongoose = require('mongoose');
+
 // index wants stuff from server
 const server = require('./lib/server.js');
 
@@ -8,7 +11,16 @@ require('dotenv').config();
 
 // port var to use port in env file
 const PORT = process.env.PORT;
+// ditto to user mongoose uri in env file
+// const MONGOOSE = process.env.MONGODB_URI;
 
+// const mongooseOptions = {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// };
+
+// mongoose.connect(MONGOOSE, mongooseOptions);
 
 // // index.js job is to be front door, and to start server. server itself will have all other stuff. so server needs something called start (method)
 server.start(PORT);
