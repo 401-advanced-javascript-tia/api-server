07587924be23d414ae1a8394d46b96e60bc1c568
Tia Low <lowtia@gmail.com>
2020-08-19 20:33:09 -0700

    Passing test for logger mw, cant quite sort timestamp mw test

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -1,20 +1,22 @@
 'use strict';
 
-const server = require('../lib/server.js');
+const {server} = require('../lib/server.js');
 const supertest = require('supertest');
+
 const mockRequest = supertest(server);
+
+let consoleSpy = jest.spyOn(console, 'log');
 
 describe('Logger Middleware', () => {
 
-  it('should log timestamp, path, method', () => {
+  it('should call console.log', () => {
 
     return mockRequest
-      // .get('/invalid')
-      // .then( results=> {
-      //   expect(results.status).toBe(404);
-      // }).catch(console.error);
+      .get('/products')
+      .then( results => {
+        expect(consoleSpy).toHaveBeenCalled();
+      }).catch(console.error);
 
   });
-  
-  
+
 });
--- __tests__/timestamp.test.js
+++ __tests__/timestamp.test.js
@@ -1,20 +1,25 @@
 'use strict';
 
-const server = require('../lib/server.js');
+const {server} = require('../lib/server.js');
 const supertest = require('supertest');
 const mockRequest = supertest(server);
 
+let consoleSpy = jest.spyOn(console, 'log');
+
 describe('Timestamp Middleware', () => {
 
-  it('should log valid timestamp', () => {
+  it('should call create instance of Date object', () => {
 
     return mockRequest
-      // .get('/invalid')
-      // .then( results=> {
-      //   expect(results.status).toBe(404);
-      // }).catch(console.error);
+      .get('/products')
+      .then( results => {
+        // expect(consoleSpy).toInclude();
+        
+        // CANT QUITE FIGURE OUT WHAT TO TEST HERE! ILL COME BACK TO IT. 
+
+        console.log('RESULTS IN TIMESTAMP TEST: ', results);
+      }).catch(console.error);
 
   });
-  
-  
+
 });
